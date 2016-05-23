68f6ab463f59709f68f320f7de043598cb11e73d
Javier Ramirez <frjaraur@gmail.com>
2016-05-23 20:15:40 +0200

    simpleapp.js - Common to all Linux OSes

---
+++ simpleapp.js
@@ -0,0 +1,16 @@
+var args = process.argv.slice(2);
+var http=require('http');
+var myip=require('my-local-ip')()
+var hostname=require('os').hostname();
+
+// Port must be define .... :| will try do it better...
+var port=args[0];
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(myip+' - '+hostname);
+  console.log("Request received from " + req.connection.remoteAddress);
+}).listen(port);
+
+console.log(myip+' - '+hostname)
+console.log('Server running at http://localhost:'+port+'/');
